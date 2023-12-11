7be9fc5ea56f86b2b60463b86f53927d432fca68
iprosenjitp <iprosenjitp@yahoo.com>
2023-12-11 23:17:50 +0600

    update index.js

--- tech-school-server/index.js
+++ tech-school-server/index.js
@@ -80,7 +80,6 @@ async function run() {
 
         app.get("/users/:id", async (req, res) => {
             const id = req.params.id;
-            console.log(id);
             const query = { _id: new ObjectId(id) }
             const result = await usersCollection.findOne(query);
             res.send(result);
